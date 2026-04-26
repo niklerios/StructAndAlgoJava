9aebe8317f1c347625d5de1e72547ef684eb9489
niklerios <web.nikler@yandex.ru>
2026-04-26 03:42:57 +0300

    refactor stack.swift

--- Stack/stack.swift
+++ Stack/stack.swift
@@ -1,56 +1,53 @@
-enum StackError: Error {
-    case overflow
-    case underflow
-}
+enum StackError: Error { case overflow, underflow, corruptedState }
 
-struct StackX<T> {
+struct Stack<T> {
     private let maxSize: Int
     private var stackArray: [T?]
     private var top = -1
 
     private var topItem: T {
-        guard let item = stackArray[top] else {
-            fatalError("Unexpected nil value at top of stack")
+        get throws {
+            guard
+                (0..<maxSize).contains(top),
+                let item = stackArray[top]
+            else {
+                throw StackError.corruptedState
+            }
+            return item
         }
-
-        return item
     }
 
     var isEmpty: Bool { top == -1 }
     var isFull: Bool { top == maxSize - 1 }
 
     init(maxSize: Int) {
+        precondition(maxSize > 0, "maxSize must be > 0")
+
         self.maxSize = maxSize
         stackArray = [T?](repeating: nil, count: maxSize)
     }
 
     mutating func push(_ item: T) throws {
-        if isFull {
-            throw StackError.overflow
-        }
+        guard !isFull else { throw StackError.overflow }
 
         top += 1
         stackArray[top] = item
     }
 
     mutating func pop() throws -> T {
-        guard !isEmpty else {
-            throw StackError.underflow
-        }
+        guard !isEmpty else { throw StackError.underflow }
 
         defer {
             stackArray[top] = nil
             top -= 1
         }
 
-        return topItem
+        return try topItem
     }
 
     func peek() throws -> T {
-        guard !isEmpty else {
-            throw StackError.underflow
-        }
-
-        return topItem
+        guard !isEmpty else { throw StackError.underflow }
+        return try topItem
     }
 }
+
